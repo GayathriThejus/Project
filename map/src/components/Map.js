91359795cfb7441c958a77a121c52c1d41fe8147
Gayathri Thejus <gayathrithejus@gmail.com>
2024-03-29 00:59:55 +0530

    map updated

--- map/src/components/Map.js
+++ map/src/components/Map.js
@@ -34,6 +34,13 @@ function Map() {
        Adhil
       </Popup>
     </Marker>
+
+    <Marker position={[12.786, 78.2144]} icon={defaultIcon}>
+      <Popup>
+       Appu
+      </Popup>
+    </Marker>
+
   </MapContainer>
   <h1>
     distance : 
